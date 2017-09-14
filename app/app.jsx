3dfe36a168b8b15c8531b50ce1afa0db92aecefe
RD <richard.thericgroup@hotmail.com>
2017-09-14 22:15:43 +1000

    add main, router

--- app/app.jsx
+++ app/app.jsx
@@ -1,11 +1,15 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-
-
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('./components/Main.jsx')
 
 
 ReactDom.render(
-    <h1>Boilerplate</h1>,
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+
+        </Route>
+    </Router>,
     document.getElementById('app')
 );
 
