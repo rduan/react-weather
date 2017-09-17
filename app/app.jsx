dd6b3770d8330ddc860436e0c7538223543077d5
RD <richard.thericgroup@hotmail.com>
2017-09-17 15:59:25 +1000

    install foundation , do not user higher version

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,11 @@ var Weather = require('./components/weather')
 var About = require('./components/About')
 var Sample = require('./components/Sample')
 
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+//require('script!foundation-sites/dist/js/foundation.min.js');
+
+
 
 ReactDom.render(
     <Router history={hashHistory}>
