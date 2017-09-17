53ded54a51c3c6993533ec0804f68c83debe6227
RD <richard.thericgroup@hotmail.com>
2017-09-18 07:00:28 +1000

    add rid system style about

--- app/components/About.js
+++ app/components/About.js
@@ -11,8 +11,18 @@ var React = require('react');
 var About = (props)=>{
     return (
         <div>
-            <h2>About</h2>
-            <p>Welcome about</p>
+            <h1 className="text-center">About</h1>
+            <p>Weather application build on React.</p>
+            <p>
+                Here are some of tools I used:
+            </p>
+            <ul>
+                <li>React</li>
+                <li>Open Weather Map</li>
+                <li>Foundation</li>
+                <li>axios</li>
+                <li>Express</li>
+            </ul>
         </div>
         
     )
